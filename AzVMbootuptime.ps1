82b02a3a1d012236ddc15fae60618cf85c8894d1
vamdevmishra <66116147+vamdevmishra@users.noreply.github.com>
2020-08-27 10:55:00 +0530

    Create AzVMbootuptime.ps1

---
+++ AzVMbootuptime.ps1
@@ -0,0 +1,70 @@
+<#
+.Synopsis
+   This cmdlet is to find the uptime of the Azure Virtual Machine
+.DESCRIPTION
+   This cmdlet is to find the uptime of the Azure Virtual Machine
+   For more information visit https://docs.microsoft.com/en-us/powershell/module/az.compute/get-azvm?view=azps-4.6.0
+.EXAMPLE
+   Get-AZVMBootTime -Name Windows10 -ResourceGroupName az104
+   Get-AzVM | %{Get-AZVMBootTime -ResourceGroupName $_.ResourceGroupName -name $_.name} |ft
+.
+#>
+function Get-AZVMBootTime
+{
+    [CmdletBinding()]
+    [Alias()]
+    [OutputType([int])]
+    Param
+    (
+        # Param1 help description
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   ValueFromPipeline=$True,
+                   Position=0)]
+        $Name,
+
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$false,
+                   Position=1)]
+        $ResourceGroupName
+    )
+
+    Process
+    {
+    Try {
+    Write-Host "checking for VM $name" -ForegroundColor Green
+    $vm=Get-AzVM -ResourceGroupName $ResourceGroupName -Name $Name -Status -ErrorAction stop
+    if($vm.Statuses[1].DisplayStatus -eq "VM deallocated" )
+    {
+    $uptime = "Not Applicable"
+    }
+    else
+    {
+    $uptime = (New-Timespan –Start (($vm.statuses[0].Time).AddHours(5.5)) –End (get-date)).minutes
+    }
+
+                $Properties =      @{Name  = $Name
+                                    OsName =$vm.OsName
+                                    ResourceGroupName =$vm.ResourceGroupName
+                                    VMStatus =$vm.Statuses[1].DisplayStatus
+                                    BootTime  = $vm.statuses[0].Time
+                                    UPtime_Minutes = $uptime
+                                    }
+                              
+                $Object = New-Object -TypeName PSObject -Property $Properties | Select Name,OsName,Resourcegroupname,vmstatus,boottime,uptime_minutes 
+                $Object
+    }
+
+    catch {}
+
+    
+
+}
+
+End
+    {
+    #Write-Host "script completed successfully" -ForegroundColor yellow 
+    }
+    }
+   
+   
