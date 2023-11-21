12fa9f4220c1188fa2fa5437b5576bc5ba032530
vamdevmishra <66116147+vamdevmishra@users.noreply.github.com>
2023-11-21 17:53:08 +0530

    Add files via upload

---
+++ gpopermsreport.ps1
@@ -0,0 +1,27 @@
+﻿$report = @()
+$allgpos=Get-GPO -All
+foreach($gpo in $allgpos)
+ {
+  
+  $gpopermissions=Get-GPPermissions -Guid $gpo.id -All
+
+  #togetthegpopermissionseperately
+    $gpoperms=@()
+    foreach($gpopermission in $gpopermissions)
+    {
+      $object = [PSCustomObject]@{
+        GPOName = $gpo.DisplayName
+        Trustee = $gpopermission.Trustee.Name
+        TrusteeType=$gpopermission.Trustee.sidtype
+        Permission=$gpopermission.Permission
+        Inherited=$gpopermission.Inherited
+        }
+
+        $gpoperms+=$object
+    }
+
+  $report+=$gpoperms
+
+ }
+
+ $report | Export-Csv -NoTypeInformation gpoperm.csv
